e149d9c72ea7a0eb919c9728917c676796032cdc
BRKFun <dzeko>
2021-12-25 19:40:14 +0300

    k8s files added

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
 
 
 @app.route('/')
